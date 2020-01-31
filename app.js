f9d045ab594bb809fe33a34e1c56736f2351e778
emmanueliyanu21 <emmanueliyanu2012@gmail.com>
2020-01-31 19:48:05 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const { MongoClient, ObjectId } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017', function (err, client) {
+    if (err) throw err;
+    const object = new ObjectId();
+    console.log(object);
+    console.log('CONNECTED');
+
+    // creating our database
+    const db = client.db('animals');
+
+    // inserting our databse
+    // db.collection('mammals').insertOne({
+    //     name: 'horse',
+    //     leg: 4
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log(err);
+    //     }
+    //     console.log('inserted');
+    // })
+
+    // updating our database
+    // db.collection('mammals').findOneAndUpdate({
+    //     _id: newObjectId('...')
+    // }, {
+    //     $set: {name: 'updated'}
+    // }).then(result =>{
+    //         console.log(result)
+    // }).catch(err => {
+    //         console.log(err)
+    //     });
+
+    // deleting our database
+    // db.collection('mammals').deleteMany({ name: 'Emmanuel Iyanu' });
+
+    // db.collection('mammals').deleteOne({ name: 'Emmanuel Iyanu' });
+
+    // db.collection('mammals').deleteAndFind({ name: 'Emmanuel Iyanu' });
+
+    db.collection('mammals').findOneAndDelete({
+        _id: newObjectId('...')
+    }).then(result => {
+        console.log(result);
+    });
+})
